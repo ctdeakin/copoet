5300d8982f872597a0ef80a37426b7fc60c67b1e
ctdeakin <ctdeakin@gmail.com>
2022-03-11 17:41:46 -0500

    feat: add webpack dev server and hot module reload

--- database/routes.js
+++ database/routes.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+
 const { Configuration, OpenAIApi } = require("openai");
 
 
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app.jsx';
+import '../public/main.css'
+
+ReactDOM.render(
+      <App />,
+    document.getElementById('root')
+  );
+
+  module.hot.accept();
