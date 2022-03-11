85dca025451347e8f56db4f377740bca01051220
ctdeakin <ctdeakin@gmail.com>
2022-03-11 15:03:33 -0500

    feat: connect to openai

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const { Configuration, OpenAIApi } = require("openai");
+
+
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration)
+
+async function completeMe(prompt) {
+    const {data} = await openai.createCompletion("text-davinci-001", {
+        prompt: prompt,
+        max_tokens: 6
+    })
+
+    console.log(data)
+} 
+
+completeMe('Say Hello World')
