e6c6645ce65266c04ffb784bb007c4a86e2da76a
ctdeakin <ctdeakin@gmail.com>
2022-03-13 15:21:38 -0400

    add: database adapters, testDb

--- database/index.js
+++ database/index.js
@@ -3,7 +3,10 @@ const CONNECTION_STRING = 'postgres://localhost:5432/copoet'
 const client = new Client(CONNECTION_STRING)
 
 module.exports = {
-    client
+    client,
+    getAllUsers,
+    getPoemsByUserId,
+    getLinesByPoemId
 }
 
 //database adapters
@@ -21,12 +24,55 @@ async function getAllUsers() {
     }
 }
 
-async function getAllPoems() {
+async function getPoemsByUserId(userId) {
     try{
         const{rows: poems} = await client.query(`
-        SELECT * FROM poems;
-        `)
+        SELECT * FROM poems
+        WHERE poems.poet=$1;
+        `, [userId])
+        return poems
     }catch(error){
         throw error
     }
 }
+
+async function getLinesByPoemId(poemId) {
+    try{
+        const{rows: lines} = await client.query(`
+            SELECT * FROM lines
+            WHERE lines.poem=$1
+        `, [poemId])
+        return lines
+    }catch(error){
+        throw error
+    }
+}
+
+
+
+async function testDb(){
+try{
+    const users = await getAllUsers();
+    console.log({users}) //annotating logs as object
+
+    const poem = await getPoemsByUserId(1);
+    console.log({poem})
+
+    const lines = await getLinesByPoemId(1)
+    console.log({lines})
+}catch(error){
+    throw(error)
+}
+
+}
+
+async function init(){
+    try{await client.connect()
+        await testDb()} catch(error){
+            throw(error)
+        }
+    
+
+}
+
+init()
