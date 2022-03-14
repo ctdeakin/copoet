9f990d45ae5374977c572c8bba3614178c158677
ctdeakin <ctdeakin@gmail.com>
2022-03-13 21:53:47 -0400

    add: poems list view

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,37 +1,67 @@
-import React from 'react'
-import axios from 'axios'
+import React, { useState } from 'react';
+import axios from 'axios';
 
+export default function Sidebar(props) {
+  const [userPoems, setUserPoems] = useState([]);
+  const [showList, setShowList] = useState(false);
 
-export default function Sidebar(props){
-
-    function reset() {
-        props.setLine('')
-        props.setPoem([])
-        props.setTitle('')
-    }
-    async function handleClick(e) {
-        switch(e.target.value){
-            case 'new':
-                reset()
-                return
-            case 'save':
-                let {poem, title} = props.draft
-                console.log(poem, title)
-                await axios.post('/api/poems', {poem, title})
-                reset()
-            case 'export':
-                
-            case 'poems':
-                await axios.get('/api/poems')
+  function reset() {
+    props.setLine('');
+    props.setPoem([]);
+    props.setTitle('');
+  }
+  async function handleClick(e) {
+    switch (e.target.value) {
+      case 'new':
+        reset();
+        return;
+      case 'save':
+        let { poem, title } = props.draft;
+        console.log(poem, title);
+        await axios.post('/api/poems', { poem, title });
+        reset();
+        return;
+      case 'export':
+        //future feature to send poem to other file type
+      case 'poems':
+        if (showList) {
+          setShowList(!showList);
+        } else {
+          const { data } = await axios.get('/api/poems');
+          setUserPoems(data.poems);
+          setShowList(!showList);
         }
     }
+  }
 
-    return(
-        <div>
-            <button onClick={handleClick} value="new">New</button>
-            <button onClick={handleClick} value="save">Save</button>
-            <button onClick={handleClick} value="export">Export</button>
-            <button onClick={handleClick} value="poems">Poems</button>
-        </div>
-    )
+  function PoemList() {
+     if(userPoems.length>0){
+            return <div>
+            {userPoems.map((poem) => (
+              <ul>{poem.title}</ul>
+            ))}
+          </div>
+        }
+    else{
+        return(<div>No Poems</div>)
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick} value="new">
+        New
+      </button>
+      <button onClick={handleClick} value="save">
+        Save
+      </button>
+      <button onClick={handleClick} value="export">
+        Export
+      </button>
+      <button onClick={handleClick} value="poems">
+        Poems
+      </button>
+      {showList && <PoemList />}
+    </div>
+  );
 }
